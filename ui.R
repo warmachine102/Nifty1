1126ef6f89510c1f22b7e1ace2beae42e8b6865c
MrNobody <swayampal81@gmail.com>
2025-03-17 15:34:07 +0530

    Create ui.R

---
+++ ui.R
@@ -0,0 +1,36 @@
+# ui.R
+
+library(shiny)
+library(DT)
+
+ui <- fluidPage(
+  titlePanel("Nifty 50 Stock Comparison"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("stock", "Select Stock (Nifty 50):",
+                  choices = c("ADANIPORTS.NS", "APOLLOHOSP.NS", "ASIANPAINT.NS", "AXISBANK.NS", "BAJAJ-AUTO.NS",
+                              "BAJFINANCE.NS", "BAJAJFINSV.NS", "BHARTIARTL.NS", "BPCL.NS", "BRITANNIA.NS",
+                              "CIPLA.NS", "COALINDIA.NS", "DIVISLAB.NS", "DRREDDY.NS", "EICHERMOT.NS",
+                              "GRASIM.NS", "HCLTECH.NS", "HDFC.NS", "HDFCBANK.NS", "HDFCLIFE.NS",
+                              "HEROMOTOCO.NS", "HINDALCO.NS", "HINDUNILVR.NS", "ICICIBANK.NS", "INDUSINDBK.NS",
+                              "INFY.NS", "JSWSTEEL.NS", "KOTAKBANK.NS", "LT.NS", "M&M.NS",
+                              "MARUTI.NS", "NESTLEIND.NS", "NTPC.NS", "ONGC.NS", "POWERGRID.NS",
+                              "RELIANCE.NS", "SBILIFE.NS", "SBIN.NS", "SHREECEM.NS", "SUNPHARMA.NS",
+                              "TATACONSUM.NS", "TATAMOTORS.NS", "TATASTEEL.NS", "TCS.NS",
+                              "TECHM.NS", "TITAN.NS", "ULTRACEMCO.NS", "UPL.NS", "WIPRO.NS"),
+                  selected = "RELIANCE.NS"),
+      dateRangeInput("dates", "Date Range:", start = Sys.Date() - 30, end = Sys.Date()),
+      actionButton("fetch", "Fetch Data")
+    ),
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Stock Data",
+                 dataTableOutput("stock_table")),
+        tabPanel("Stock Plot",
+                 plotOutput("stock_plot")),
+        tabPanel("Comparison Plot",
+                 plotOutput("comparison_plot"))
+      )
+    )
+  )
+)
